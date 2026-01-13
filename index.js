200507f5e88146386ccf45bef6e68172db2b4d6b
Jameson-Ryan <jamrya0726@students.maquoketaschools.org>
2026-01-13 14:33:03 -0600

    Enhance calculateSquare to update DOM elements
    Update calculateSquare function to display results in the DOM.

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ function calculateSquare(){
 
     //add code here
     value *= value;
+    
     if (isNaN(value)) {
         result = 'Invalid, please enter a number';
     } else {
         result = value;
     }
+
+    document.getElementById('number').value = value;
+    document.getElementById('result').innerHTML = result;
 }
+
